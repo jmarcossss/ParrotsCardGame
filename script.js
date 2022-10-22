2924a94c04d337ccd945a3e70da7ae1246a5f2ca
jmarcossss <jmav@cin.ufpe.br>
2022-10-22 04:05:23 -0300

    função que insere as cartas na tela OK

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let num_cartas = 0;
 let bool = 1;
+let i = 0;
+const gifsSelecionados = [];
+const armazena_gifs = [
+    'bobrossparrot.gif',
+    'explodyparrot.gif',
+    'fiestaparrot.gif',
+    'metalparrot.gif',
+    'revertitparrot.gif',
+    'tripletsparrot.gif',
+    'unicornparrot.gif'
+]
 
-
-function entrarNoJogo() {
+// Função que verifica se a quantidade de cartas que o usuário escolheu é válida!
+function entrarNoJogo() { 
     while(bool){
         num_cartas = prompt("Quantas cartas você deseja colocar?");
         if(num_cartas > 3 && num_cartas < 15 && (num_cartas % 2) === 0){
@@ -15,6 +26,17 @@ function entrarNoJogo() {
     }
 };
 
-entrarNoJogo();
 
-alert("OK!");
+//Função que vai inserir a quantidade de cartas que o usuário selecionou
+function inserirCartas() {
+    const carta_inserida = document.querySelector(".cartas");
+    carta_inserida.innerHTML += `<div class="barreira"></div>`
+    for(i = 0; i < num_cartas; i++){
+        carta_inserida.innerHTML += `<div class="carta" onclick=lado_oposto(this)>
+        <img class="foto" src='./Arquivos/back.png' />
+        <img class="foto verso" src='Arquivos/${armazena_gifs[i]}' />
+        </div>`
+    }
+};
+
+
