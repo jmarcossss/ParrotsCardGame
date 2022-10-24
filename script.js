f2e795bd0f3ae250a13cf225408b7f5c2806eac8
jmarcossss <jmav@cin.ufpe.br>
2022-10-24 17:40:24 -0300

    Quase finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let num_cartas = 0;
 let bool = 1;
 let i = 0;
 let qtd_de_jogadas = 0;
-let primeira_carta, segunda_carta;
+let carts_comp_first, cards_comp_second;
 let paresFeitos = []
 const armazena_gifs_func = [];
 const armazena_gifs = [ //Dobrar os gifs porque para cada par de carta, são necessários gifs iguais...
@@ -36,17 +36,18 @@ function entrarNoJogo() {
     }
 };
 
-function random_gifs() {
-    for(let i = 0; i < num_cartas; i++){
-        gifsSelecionados.push(armazena_gifs[i]) //Armeza os gifs num novo array
-        gifsSelecionados.sort(comparador) //Sortear os gifs
-    }
-}
+// Funções que geram valores aleatórios e armazenam em armazena_gifs_func
 
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
+function random_gifs() {
+    for(let i = 0; i < num_cartas; i++){
+        armazena_gifs_func.push(armazena_gifs[i]); //Armeza os gifs num novo array
+        armazena_gifs_func.sort(comparador); //Sortear os gifs
+    }
+}
 
 //Função que vai inserir a quantidade de cartas que o usuário selecionou
 function inserirCartas() {
@@ -57,8 +58,8 @@ function inserirCartas() {
         carta_inserida.innerHTML += 
         `<div class="carta" onclick=lado_oposto(this)>
             <img class="foto frente" src='./Arquivos/back.png' />
-            <img class="foto verso" src='Arquivos/${gifsSelecionados[i]}' />
-        </div>`
+            <img class="foto verso" src='Arquivos/${armazena_gifs_func[i]}' />
+        </div>`;
     }
 };
 
@@ -68,44 +69,49 @@ function lado_oposto(button) { //O button recebe o valor do this de inserirCarta
     qtd_de_jogadas++;
     button.querySelector(".verso").classList.add("virar-verso");
     button.querySelector(".frente").classList.add("virar-frente");
-    if(!document.querySelector(".primeira_carta")){ //Inicialmente aqui vai ter um valor falso, pois não recebemos o primeira_carta de vez
-        button.classList.add("primeira_carta")      //Logo, o operador not(!) irá 
-        primeira_carta = button
-        primeira_carta.setAttribute('onclick','')
+    if(!document.querySelector(".carts_comp_first")){ //Inicialmente aqui vai ter um valor falso, pois não recebemos o carts_comp_first de vez
+        button.classList.add("carts_comp_first");      //Logo, o operador not(!) irá 
+        carts_comp_first = button;
+        carts_comp_first.setAttribute('onclick','');
     }
     else{
         document.querySelector(".barreira").style.display = "block";    
-        button.classList.add("segunda_carta");
-        segunda_carta = button
-        setTimeout(compararPar, 500)
+        button.classList.add("cards_comp_second");
+        cards_comp_second = button;
+        setTimeout(compararPar, 500);
     }
 }
 
 function compararPar(){
-    if(primeira_carta.innerHTML !== segunda_carta.innerHTML){ //Se as cartas comparadas forem diferentes, quer dizer que aquele par está errado
-        primeira_carta.querySelector(".verso").classList.remove("virar-verso")
-        primeira_carta.querySelector(".frente").classList.remove("virar-frente")
-        segunda_carta.querySelector(".verso").classList.remove("virar-verso")
-        segunda_carta.querySelector(".frente").classList.remove("virar-frente")
-        primeira_carta.setAttribute("onclick", "lado_oposto(this)")
-        primeira_carta.classList.remove("primeira_carta")
-        segunda_carta.classList.remove("segunda_carta")
+    if(carts_comp_first.innerHTML !== cards_comp_second.innerHTML){ //Se as cartas comparadas forem diferentes, quer dizer que aquele par está errado
+        if(bool == 0) {
+            bool = 1;
+            if(bool = 1){
+                carts_comp_first.querySelector(".verso").classList.remove("virar-verso");
+                carts_comp_first.querySelector(".frente").classList.remove("virar-frente");
+                cards_comp_second.querySelector(".verso").classList.remove("virar-verso");
+                cards_comp_second.querySelector(".frente").classList.remove("virar-frente");
+                carts_comp_first.setAttribute("onclick", "lado_oposto(this)");
+                carts_comp_first.classList.remove("carts_comp_first");
+                cards_comp_second.classList.remove("cards_comp_second");
+            }
+        }
     }
     else{ //Caso contrário, elas são iguais
-        paresFeitos.push("Feito")
-        primeira_carta.classList.remove("primeira_carta")
-        segunda_carta.classList.remove("segunda_carta")
-        primeira_carta.setAttribute('onclick','')
-        segunda_carta.setAttribute('onclick','')
+        paresFeitos.push(".");
+        carts_comp_first.classList.remove("carts_comp_first");
+        cards_comp_second.classList.remove("cards_comp_second");
+        carts_comp_first.setAttribute('onclick','');
+        cards_comp_second.setAttribute('onclick','');
         sairDoJogo();
 
     }
-    document.querySelector(".barreira").style.display = "none"
+    document.querySelector(".barreira").style.display = "none";
 }
 
 function sairDoJogo() {
     if(paresFeitos.length === (num_cartas / 2)){
-                alert(`Parabens, você ganhou ${qtd_de_jogadas} jogadas!!!`)
+                alert(`Você ganhou em ${qtd_de_jogadas} jogadas!`);
             }
 }
 
